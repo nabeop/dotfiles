29c096f4d2b939484a6f73fb2ff052e3641fe6ef
nabeop <kt.mynb@gmail.com>
2014-06-12 15:40:01 +0900

    add .latexmkrc

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gvimrc ~/.gvimrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/Brewfile ~/Brewfile
+ln- s ~/dotfiles/.latexmkrc ~/.latexmkrc
