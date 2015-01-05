ae51a89b85479773183364cb0d2805c9dd0581ae
nabeop <kt.mynb@gmail.com>
2015-01-05 11:26:28 +0900

    add zsh profiles

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -5,3 +5,4 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/Brewfile ~/Brewfile
 ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
 ln -s ~/dotfiles/karabiner ~/Library/Application\ Support/Karabiner
+cp ~/dotfiles/.zshrc ~/.zshrc
